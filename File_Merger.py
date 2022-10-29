ea826ebcd7dfba0c9384b8d1b6512ab27bf581cc
Georgy Galkin <115151263+Georgy-Galkin@users.noreply.github.com>
2022-10-29 19:39:44 +0300

    Update File_Merger.py

--- File_Merger.py
+++ File_Merger.py
@@ -11,7 +11,7 @@ layout = [
     
     [sg.T("Input Storage Folder:", s=25,justification="r"), sg.I(key="-FOLDER-"), sg.FolderBrowse()],
     [sg.Text('Specify Sheet Name', size =(25, 1),justification="r"),sg.I(key="-IN-")],
-    [sg.Text('Specify File Type in lowercase', size =(25, 1),justification="r"),sg.I(key="-FILE-")], 
+    
     [sg.Submit()],
     [sg.Text('© JTI', size =(80, 1),justification="r")]
     
@@ -24,23 +24,25 @@ event, values = window.read()
 
 result_file=str(values['-FOLDER-'])
 sheet=str(values['-IN-'])
-file=str(values['-FILE-'])
+
 window.close()
 
 result_file=result_file.replace("\\","\\\\")
 
 
 
-all_files=glob.glob(result_file+"/*."+file)
+all_files=glob.glob(result_file+"/*.xls*")
 li=[]
 
 
 for filename in all_files:
     try:
         print("Reading ",filename)
-        sales=pd.read_excel(filename,sheet_name=sheet )
+        
+        sales = pd.read_excel(filename, sheet_name=sheet)
+        
         print("Reading ",sheet)
-        filename=filename.replace("."+file,"")
+        filename=filename.replace("."+filename[-4:],"")
         filename=filename.replace(result_file,"")
         
         sales["DATE"]=filename[-4:]
@@ -54,7 +56,10 @@ for filename in all_files:
         print("Appending File")
         print("--------------")
     except:
-        print(filename,"ERROR, smth went wrong")
+        
+        print(filename)
+        print("ERROR, smth went wrong")
+        print("-----")
 print("Appending all dataframes")
 frame=pd.concat(li,axis=0,ignore_index=True)
 print("Total Rows Amount: ",frame.shape[0])
